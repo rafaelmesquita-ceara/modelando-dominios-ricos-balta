07b80ec13a9d21bae72e4f16cb02bd9959154436
Rafael Mesquita <rafaelempresarialacc@gmail.com>
2021-03-01 21:07:27 -0300

    Corrupção de código

---
+++ PaymentContext/PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+  [TestClass]
+  public class StudantTests
+  {
+    [TestMethod]
+    public void AdicionarAssinatura()
+    {
+      var subscription = new Subscription(null);
+      var student = new Student("Rafael", "Mesquita", "123456789", "rafael.io");
+      student.AddSubscription(subscription);
+    }
+  }
+}
